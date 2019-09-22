e40823d4db2971aa7784487db22b6f06a3cdda8d
Siarhei <spartateambuilder@mail.ru>
2019-09-22 15:51:27 +0300

    Updates

---
+++ precache-manifest.e3edd586d6599e302c4e3a2dfb59e581.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f05f727bda7a8ba7ce297a69986ee84",
+    "url": "/TestWeather/index.html"
+  },
+  {
+    "revision": "320905816da4603280bf",
+    "url": "/TestWeather/static/css/main.0468ed6f.chunk.css"
+  },
+  {
+    "revision": "3404fd6e6dc2f9d807c4",
+    "url": "/TestWeather/static/js/2.348ed4e8.chunk.js"
+  },
+  {
+    "revision": "320905816da4603280bf",
+    "url": "/TestWeather/static/js/main.f4dfbdcf.chunk.js"
+  },
+  {
+    "revision": "bed979865a0b74765a0b",
+    "url": "/TestWeather/static/js/runtime~main.d9ab7df1.js"
+  }
+]);
